313df769019b4c3861ce07426709bbf6a77a2a6c
INGRYD SHIRLLEY DA SILVA RIBEIRO <22282092@CFP127ALUNO.LOCAL>
2023-05-24 08:25:16 -0300

    rotas

---
+++ js/rounter.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const routes = {
+    '/sobre_nos': '/pages/sobre-nos.html',
+    '/projetos': '/pages/projetos.html',
+    '/junte_se': '/pages/junte-se.html',
+}
+
+const route = async() => {
+    window.event.preventDefault()
+    window.history.pushState({}, "", window.event.target.href)
+
+    const path = window.location.pathname
+
+    const route = routes[path]
+
+    const response = await fetch(route)
+    const html = await response.text()
+
+    document.getElementById('root').innerHTML = html
+
+}
+
+
+
+
+// const teste = document.getElementById('#home')
+// teste.addEventListener('click', handleClick)
+
+window.route = route
