8a030127766351a74f66b3e125f5e607336291b1
Leticia Evelin <ljkeleticia@gmail.com>
2023-06-07 21:44:08 -0300

    forms prontinhos

--- js/post_doador.js
+++ js/post_doador.js
@@ -56,8 +56,68 @@ const data_nascimento = document.getElementById('input-nascimento-doador');
 const telefone = document.getElementById('input-telefone-doador');
 const button = document.getElementById('submit-button-doador');
 
+
+function setErrorFor(input, message) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector('.error-message');
+
+  // Adiciona a mensagem de erro
+  small.innerText = message;
+
+  // Adiciona a classe de erro
+  formControl.classList.remove('success');
+  formControl.classList.add('error');
+}
+
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+
+  // Adicionar a classe de sucesso
+  formControl.classList.remove('error');
+  formControl.classList.add('success');
+}
+
+function checkEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
+function checkInputs() {
+  if (nome.value === '') {
+    setErrorFor(nome, 'O nome é obrigatório!');
+  } else {
+    setSuccessFor(nome);
+  }
+
+  if (email.value === '') {
+    setErrorFor(email, 'O email é obrigatório!');
+  } else if (!checkEmail(email.value)) {
+    setErrorFor(email, 'Por favor, insira um email válido!');
+  } else {
+    setSuccessFor(email);
+  }
+
+  if (cpf.value === '') {
+    setErrorFor(cpf, 'O CPF é obrigatório!');
+  } else {
+    setSuccessFor(cpf);
+  }
+
+  if (telefone.value === '') {
+    setErrorFor(telefone, 'O telefone é obrigatório!');
+  } else {
+    setSuccessFor(telefone);
+  }
+
+  if (data_nascimento.value === "") {
+    setErrorFor(data_nascimento, "A data de nascimento é obrigatória!");
+  } else {
+    setSuccessFor(data_nascimento);
+  }
+}
+
 button.addEventListener('click', async (e) => {
   e.preventDefault();
+  checkInputs()
 
   const selectElement = document.getElementById('preco_doacao');
   const selectValue = selectElement.value;
@@ -89,63 +149,8 @@ button.addEventListener('click', async (e) => {
       console.log('Dados da doação enviados com sucesso.');
     }
   } catch (error) {
-    console.error('Ocorreu um erro:', error);
+    console.error(error);
   }
 });
-
-
-// function setErrorFor(input, message) {
-//   const formControl = input.parentElement;
-//   const small = formControl.querySelector('.error-message');
-
-//   // Adiciona a mensagem de erro
-//   small.innerText = message;
-
-//   // Adiciona a classe de erro
-//   formControl.classList.remove('success');
-//   formControl.classList.add('error');
-// }
-
-// function setSuccessFor(input) {
-//   const formControl = input.parentElement;
-
-//   // Adicionar a classe de sucesso
-//   formControl.classList.remove('error');
-//   formControl.classList.add('success');
-// }
-
-// function checkEmail(email) {
-//   return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
-// }
-
-// function checkInputs() {
-//   if (razaoSocial.value === '') {
-//     setErrorFor(razaoSocial, 'A razão social da empresa é obrigatória!');
-//   } else {
-//     setSuccessFor(razaoSocial);
-//   }
-
-//   if (cnpj.value === '') {
-//     setErrorFor(cnpj, 'O CNPJ é obrigatório!');
-//   } else {
-//     setSuccessFor(cnpj);
-//   }
-
-//   if (email.value === '') {
-//     setErrorFor(email, 'O email é obrigatório!');
-//   } else if (!checkEmail(email.value)) {
-//     setErrorFor(email, 'Por favor, insira um email válido!');
-//   } else {
-//     setSuccessFor(email);
-//   }
-
-//   if (telefone.value === '') {
-//     setErrorFor(telefone, 'O telefone é obrigatório!');
-//   } else {
-//     setSuccessFor(telefone);
-//   }
-// }
-
-
 
 
