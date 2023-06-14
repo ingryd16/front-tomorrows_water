a5ef1500c71459e86d8040fb0e05397e81924666
unknown <laylagiovanna27@gmail.com>
2023-06-14 16:35:35 -0300

    Ajustando responsividade e lighthouse

--- js/footer.js
+++ js/footer.js
@@ -1,16 +1,16 @@
-function changeFooterColor() {
-    var footer = document.getElementById("footer");
+// function changeFooterColor() {
+//     var footer = document.getElementById("footer");
   
-    // Obtém o nome da página atual
-    var pageName = window.location.pathname.split("/").pop();
+//     // Obtém o nome da página atual
+//     var pageName = window.location.pathname.split("/").pop();
   
-    // Define a cor do footer com base no nome da página
-    if (pageName === "index.html") {
-      footer.style.backgroundColor = "white";
-    } else {
-      footer.style.backgroundColor = "#EFEFEF";
-    }
-  }
+//     // Define a cor do footer com base no nome da página
+//     if (pageName === "index.html") {
+//       footer.style.backgroundColor = "white";
+//     } else {
+//       footer.style.backgroundColor = "#EFEFEF";
+//     }
+//   }
   
-  document.addEventListener("DOMContentLoaded", changeFooterColor);
+//   document.addEventListener("DOMContentLoaded", changeFooterColor);
   
